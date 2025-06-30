83d0623b6f6b9ada1d5dd37e63060c0f08f4da29
mxtlrr <ruson0914@gmail.com>
2025-06-30 15:35:29 -0500

    fix up banding.c

--- banding.c
+++ banding.c
@@ -3,15 +3,13 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <math.h>
+
+#define MAX_AMOUNT 7
 
 typedef struct {
-  uint8_t r, g, b, a;
+  uint8_t r, g, b;
 } Color;
-
-bool are_colors_the_same(Color a, Color b){
-  return (a.r == b.r) && (a.g == b.g) && (a.b == b.b);
-}
 
 uint32_t conv_to_32(Color c){
   return (c.r << 16) | (c.g << 8) | c.b;
@@ -36,26 +34,20 @@ int main(int argc, char** argv){
   uint32_t color2 = strtol(argv[2], NULL, 16);
 
   printf("[ok]: using %06x -> %06x\n", color1, color2);
+	printf("[ok]: making %02d bands\n", MAX_AMOUNT);
 
-  Color c = (Color){color1 >> 16, (color1 & 0xFFFF) >> 8, color1 & 0xFF, 255};
-  Color c2 = (Color){color2 >> 16, (color2 & 0xFFFF) >> 8, color2 & 0xFF, 255 };
-  for(uint32_t frame = 0; ; frame++){
-    for(uint16_t i = 0; i <= 640; i++){
-      if(i % (640/4) == 0 && frame % 30 == 0){
-        if(c.r      < c2.r) c.r++;
-        else if(c.r > c2.r) c.r--;
+  Color c  = (Color){color1 >> 16, (color1 & 0xFFFF) >> 8, color1 & 0xFF};
+  Color c2 = (Color){color2 >> 16, (color2 & 0xFFFF) >> 8, color2 & 0xFF};
 
-        if(c.g      < c2.g) c.g++;
-        else if(c.g > c2.g) c.g--;
+	uint8_t dR = (abs(c2.r - c.r)) / MAX_AMOUNT; uint8_t dG = (abs(c2.g - c.g)) / MAX_AMOUNT;
+	uint8_t dB = (abs(c2.b - c.b)) / MAX_AMOUNT;
 
-        if(c.b      < c2.b) c.b++;
-        else if(c.b > c2.b) c.b--;
-
-        uint32_t conv = conv_to_32(c);
-        if(are_colors_the_same(c,c2)) return 0;
-        printf("%#06x,\n", conv);
-      }
-    }
-  }
-  return 0;
+	printf("dR => %d // dG => %d // dB => %d\n", dR, dG, dB);
+	for(uint8_t i = 0; i < MAX_AMOUNT; i++){
+		c.r += (( (c2.r - c.r) < 0 ) ? -1 : 1) * dR;
+		c.g += (( (c2.g - c.g) < 0 ) ? -1 : 1) * dG;
+		c.b += (( (c2.b - c.b) < 0 ) ? -1 : 1) * dB;
+		printf("0x%06x,\n", conv_to_32(c));
+	}
+	return 0;
 }
